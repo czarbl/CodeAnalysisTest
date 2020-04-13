b8ea1d2fca625a9e26d0bf3f8e94527755667834
czarbl <46700560+czarbl@users.noreply.github.com>
2020-04-13 18:55:12 -0500

    xxx
    xxx

---
+++ src/main/java/com/sabiosolutions/code/analysis/test/CodeAnalysisTest.java
@@ -0,0 +1,13 @@
+package com.sabiosolutions.code.analysis.test;
+
+public class CodeAnalysisTest {
+
+	public static void main(String args[]) {
+		int i = 0;
+		while (true) {  //Condición trivial: siempre cierta
+			i++;
+			System.out.println ("Valor de i: " + i);
+		} 
+	}
+
+}
